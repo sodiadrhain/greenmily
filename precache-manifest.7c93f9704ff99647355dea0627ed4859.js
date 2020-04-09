94cb4fb7054afe89a2cda1270065111bb0919e8c
sodiadrhain <adesojiawobajo@gmail.com>
2020-04-09 15:23:33 +0100

    Updates

---
+++ precache-manifest.7c93f9704ff99647355dea0627ed4859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47c2be6fa6e517451685409b663a5e3",
+    "url": "%20//sodiadrhain.github.io/greenmily/index.html"
+  },
+  {
+    "revision": "07c5de2d504e7cf9a7df",
+    "url": "%20//sodiadrhain.github.io/greenmily/static/js/2.a552a814.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "%20//sodiadrhain.github.io/greenmily/static/js/2.a552a814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968620c4338a2e8b709e",
+    "url": "%20//sodiadrhain.github.io/greenmily/static/js/main.1fab47ef.chunk.js"
+  },
+  {
+    "revision": "7ed0901647b2e8816376",
+    "url": "%20//sodiadrhain.github.io/greenmily/static/js/runtime-main.59123d3b.js"
+  }
+]);
